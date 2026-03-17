c904c34ff51475f45a5f385d50a8d2aaefe8eaec
Kiana Kavoosi <kikavoosi@aol.com>
2026-03-17 15:36:48 -0700

    add tailwind and coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import logo from './assets/frootfairy.png';
 
 function App() {
 
   return (
-    <>
-      Coming soon
+    <div className='flex flex-col items-center justify-center bg-background h-full' data-theme='neutral'>
+    <img src={logo} className='max-w-100'/>
+    <p className='text-foreground'>frooty things coming soon</p>
           
-    </>
+    </div>
   )
 }
 
