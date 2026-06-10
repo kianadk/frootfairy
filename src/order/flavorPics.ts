7059db5a673ea88eac4be758d4f44c73b5d76555
Kiana Kavoosi <kikavoosi@aol.com>
2026-06-09 23:16:44 -0700

    naming

--- src/order/flavorPics.ts
+++ src/order/flavorPics.ts
@@ -2,14 +2,14 @@ import plumJam from '../assets/plumJam.jpg';
 import cherryJam from '../assets/cherryJam.png';
 import strawberryJam from '../assets/strawberryJam.png';
 import apricotJam from '../assets/apricotJam.png';
-import cherryJalapeñoJam from '../assets/cherryJalapeñoJam.jpg'
+import cherryJalapenoJam from '../assets/cherryJalapenoJam.jpg'
 import { Flavor } from '@/inventory';
 
 const flavorPics: Record<Flavor, string> = {
     'apricot': apricotJam,
     'strawberry': strawberryJam,
     'cherry': cherryJam,
-    'cherry jalapeño': cherryJalapeñoJam
+    'cherry jalapeño': cherryJalapenoJam
 };
 
 export default flavorPics;
